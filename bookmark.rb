f84817827aa458f0fffa85df11452cce7af3b880
Gus Cavanaugh <gustafcavanaugh@gmail.com>
2014-04-15 21:19:35 -0400

    Corrected Mistakes (Fingers Crossed)

--- bookmark.rb
+++ bookmark.rb
@@ -2,6 +2,12 @@ class Bookmark
  attr_accessor :url, :title
  attr_reader :last_visited
  
+ def intialize(url, title)
+  @url = url
+  @title = title
+  @last_visited = Time.now
+ end
+  
  def visit!
   last_visited = Time.now
  end
